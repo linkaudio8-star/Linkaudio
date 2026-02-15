cc95d723313924b09b2260b39ca4a9f60c73640e
Daniil Soloviov <daniil-soloviov@Ralabs-Admins-MacBook-Air.local>
2026-02-15 13:45:58 +0200

    Add ESLint and Prettier checks to CI

---
+++ .eslintrc.cjs
@@ -0,0 +1,41 @@
+module.exports = {
+  root: true,
+  ignorePatterns: [
+    "node_modules/",
+    ".home/",
+    "data/",
+    "data_tmp/",
+    "public/vendor/",
+    "*.log",
+  ],
+  overrides: [
+    {
+      files: ["server.js", "storage.js", "scripts/**/*.js"],
+      env: {
+        node: true,
+        es2022: true,
+      },
+      extends: ["eslint:recommended"],
+      globals: {
+        fetch: "readonly",
+      },
+      rules: {
+        "no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+      },
+    },
+    {
+      files: ["public/**/*.js"],
+      env: {
+        browser: true,
+        es2022: true,
+      },
+      extends: ["eslint:recommended"],
+      globals: {
+        ggwave_factory: "readonly",
+      },
+      rules: {
+        "no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+      },
+    },
+  ],
+};
